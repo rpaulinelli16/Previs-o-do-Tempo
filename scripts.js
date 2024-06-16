0800882d8e035b07741033a7c80b40be05f89f4e
Rodrigo Paulinelli <167376931+rpaulinelli16@users.noreply.github.com>
2024-06-16 17:42:48 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+const key = "1c1b52b672b1773341214b030a5cedf5"
+
+
+function colocarDadosNaTela (dados) {
+    console.log (dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector("img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+
+}
+
+
+async function buscarCidade(cidade) {
+
+const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    
+colocarDadosNaTela(dados)
+
+}
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+
+    buscarCidade(cidade)
+}
+
+//Rodrigo Paulinelli D24585
